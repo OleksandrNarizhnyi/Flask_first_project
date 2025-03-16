2c6643d38bdd1dbe8a23d7ee02de91c04dfa5c43
ssani4 <ssani4@icloud.com>
2025-03-16 10:22:29 +0100

    HW 2

--- HW/HW_2.py
+++ HW/HW_2.py
@@ -26,7 +26,7 @@
 # Валидация:
 # Проверка, что если пользователь указывает, что он занят (is_employed = true), его возраст должен быть от 18 до 65 лет.
 
-from pydantic import BaseModel, Field, EmailStr, field_validator, model_validator, ValidationError
+from pydantic import BaseModel, Field, EmailStr, model_validator, ValidationError
 from typing import Any
 
 class Address(BaseModel):
@@ -70,17 +70,6 @@ json_data = [
         "city": "New York",
         "street": "5th Avenue",
         "house_number": 123
-        }
-    }""",
-    """"{
-    "name": "Alice Smith",
-    "age": 34,
-    "email": "alice.smith@gmail.com",
-    "is_employed": true,
-    "address": {
-        "city": "Los Angeles",
-        "street": "Sunset Boulevard",
-        "house_number": 456
         }
     }""",
     """{
