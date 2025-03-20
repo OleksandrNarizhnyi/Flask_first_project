a86caf2099ca4ad0e1ac2bc7e0767921b7c688bd
ssani4 <ssani4@icloud.com>
2025-03-20 18:45:05 +0100

    Task 2

--- HW/HW_4.py
+++ HW/HW_4.py
@@ -55,21 +55,31 @@ Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 # 1. Наполнение данными
 # Добавьте в базу данных категории и продукты.
+# with Session() as session:
+#
+#     category1 = Category(id=1, name="Electronics", description="Gadgets and devices.")
+#     category2 = Category(id=2, name="Books", description="Printed books and e-books.")
+#     category3 = Category(id=3, name="Clothing", description="Clothing for men and women.")
+#
+#     product1 = Product(id=1, name="Smartphone", price=299.99, in_stock=True, category=category1)
+#     product2 = Product(id=2, name="Laptop", price=499.99, in_stock=True, category=category1)
+#     product3 = Product(id=3, name="Sci-Fi Novel", price=15.99, in_stock=True, category=category2)
+#     product4 = Product(id=4, name="Jeans", price=40.50, in_stock=True, category=category3)
+#     product5 = Product(id=5, name="T-Shirt", price=20.00, in_stock=True, category=category3)
+#
+#     session.add_all([
+#                 category1, category2, category3,
+#                 product1, product2, product3, product4, product5
+#             ])
+#     session.commit()
+#     print("Database populated successfully!")
+#====================================================================================================
+# 2. Извлеките все записи из таблицы categories. Для каждой категории извлеките и выведите все связанные с
+# ней продукты, включая их названия и цены.
 with Session() as session:
+    all_from_category = session.query(Category).all()
 
-    category1 = Category(id=1, name="Electronics", description="Gadgets and devices.")
-    category2 = Category(id=2, name="Books", description="Printed books and e-books.")
-    category3 = Category(id=3, name="Clothing", description="Clothing for men and women.")
-
-    product1 = Product(id=1, name="Smartphone", price=299.99, in_stock=True, category=category1)
-    product2 = Product(id=2, name="Laptop", price=499.99, in_stock=True, category=category1)
-    product3 = Product(id=3, name="Sci-Fi Novel", price=15.99, in_stock=True, category=category2)
-    product4 = Product(id=4, name="Jeans", price=40.50, in_stock=True, category=category3)
-    product5 = Product(id=5, name="T-Shirt", price=20.00, in_stock=True, category=category3)
-
-    session.add_all([
-                category1, category2, category3,
-                product1, product2, product3, product4, product5
-            ])
-    session.commit()
-    print("Database populated successfully!")
+    for cat in all_from_category:
+        print(f"Категория: {cat.name}, Описание: {cat.description}")
+        for product in cat.products:
+            print(f" Product name: {product.name}, price: {product.price}, available: {product.in_stock}")
